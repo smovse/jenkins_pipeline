d0f0718227a505a1d638fddc65ae809006f0a097
thj <thj@kmd.dk>
2018-06-06 13:30:45 +0200

    struts2 app

---
+++ src/main/java/dk/thj/jenkins/action/IndexAction.java
@@ -0,0 +1,9 @@
+package dk.thj.jenkins.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport {
+    public String execute() {
+        return SUCCESS;
+    }
+}
